0f6fe7fe7cace81ca86713f4ecefe2b81c11492d
puramvijayalakshmi <puramvijayalakshmi85@gmail.com>
2024-06-13 16:41:19 +0530

    project created

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi");
+            string[] a = { "vijju","Likitha","alli"};
+            for(int i = 0; i < a.Length; i++) 
+            { 
+                    string s = a[i];
+                   if (s.Contains("i"))
+                {
+                    Console.WriteLine(s);
+                }
+                   else
+                {
+                    Console.WriteLine("nothing");
+                }
+                   Console.ReadKey();
+            }
+        }
+    }
+}
